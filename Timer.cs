7fe62fd2fc6a97530cdd3ddcb3164df0f5261efd
Mark Boon <Boomslet@users.noreply.github.com>
2018-05-07 23:09:58 +0800

    Update Timer.cs
    I haven't actually checked the correctness of this but idk

--- Timer.cs
+++ Timer.cs
@@ -5,22 +5,20 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
-	//ingame text for your timer
+	[Header("Timer Prefab")]
 	public Text timerText;
-	private float startTime;
-	public float endGameTime = 15.00f;
+	
+	[Header("Timer Duration")]
+	public float endGameTimer = 15.00f;
+	
+	private float timer = 0f;
 
-	// Use this for initialization
-	void Start () {
-		startTime = Time.time;
-	}
 	// Update is called once per frame
 	void Update () {
-		float t = Time.time - startTime;
-		string minutes = ((int)t / 60).ToString ();
-		string seconds = (t % 60).ToString ("f2");
-		timerText.text = minutes + ":" + seconds;
-		if (t >= endGameTime) 
+		timer += Time.deltaTime;
+		timerText.text = timer.ToString("00.00");
+		
+		if (timer >= endGameTimer) 
 		{
 			// Your Condition
 		}
